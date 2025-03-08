5d980c1ded09211964546324c2c29687ec8c9c0d
dipenbhat557 <pandeypawan2061@gmail.com>
2025-03-09 05:04:45 +0530

    feat: nav ui done

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,5 +1,9 @@
-import Image from "next/image";
+import Nav from "@/components/Nav";
 
 export default function Home() {
-  return <div className="">hi</div>;
+  return (
+    <div className="">
+      <Nav />
+    </div>
+  );
 }
